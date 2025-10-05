d170a5e8321f2be860a16da6089c7ea6d3cac49a
Syrnikov Kirill <kirill.syrnikov@gmail.com>
2025-10-05 22:47:15 +0500

    Stage 2 of the task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.53.0"
+    id("application")
 }
 
 group = "hexlet.code"
@@ -14,6 +15,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    // Указываем главный класс
+    mainClass.set("hexlet.code.App")
 }
 
 tasks.test {
